30d87ef438edc3878c9e9c2b65f6cbc75f60c8d0
Alex Rozenman <rozenman@gmail.com>
2024-04-30 11:43:40 +0300

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -8,33 +8,87 @@ public:
     int val;
 
 public:
-    A() : val{} { printf("A()\n"); }
-    A(int val) : val{val} { printf("A(int)\n"); }
-    A(const A& a) : val{a.val} { printf("A(const A&)\n"); }
-    A(A&& a) : val{a.val} { printf("A(A&&)\n"); }
-    A& operator=(const A& a) { val = a.val; printf("operator=(const A&)\n"); return *this; }
-    A& operator=(A&& a) { val = a.val; printf("operator=(A&&)\n"); return *this; }
-    ~A() { printf("~A()\n"); }
+    A() : val{} {
+        printf("A()\n");
+    }
+
+    A(int val) : val{val} {
+        printf("A(int); // val=%d\n", val);
+    }
+
+    A(const A& a) : val{a.val} {
+        printf("A(const A&); // val=%d\n", val);
+    }
+
+    A(A&& a) : val{a.val} {
+        printf("A(A&&); // val=%d\n", val);
+    }
+
+    A& operator=(const A& a) {
+        val = a.val;
+        printf("operator=(const A&); val=%d\n", val);
+        return *this;
+    }
+
+    A& operator=(A&& a) {
+        val = a.val;
+        printf("operator=(A&&); val=%d\n", val);
+        return *this;
+    }
+
+    ~A() {
+        printf("~A(); // val=%d\n", val);
+    }
 };
 
 void func1(A a) {
-    printf("func1, %d\n", a.val);
+    printf("func1 a.val=%d\n", a.val);
 }
 
-A func2() {
-    printf("func2\n");
+void func2(const A& a) {
+    printf("func2 a.val=%d\n", a.val);
+}
+
+A func3() {
+    printf("func3\n");
     A rv = 2;
     return rv;
 }
 
+A obj(90);
+
 int main()
 {
+    printf(" - begin - \n");
+
     A x, y, z(10);
+
+    printf(" - 2 - \n");
+
     A t = x;
+
+    printf(" - 3 - \n");
+
     func1(t);
+
+    printf(" - 4 - \n");
+
     func1(std::move(z));
-    A xx(func2());
+
+    printf(" - 5 - \n");
+
+    func2(t);
+
+    printf(" - 6 - \n");
+
+    A xx(func3());
+
     printf("xx.val %d\n", xx.val);
-    y = func2();
+    printf(" - 7 - \n");
+
+    y = func3();
+
     printf("y.val %d\n", y.val);
+
+    printf(" - end - \n");
 }
